440b960ba79a98beebe541b7bb416f94e6c8ddbe
thangdv <duongthang.1903@gmail.com>
2025-04-11 11:19:55 +0700

    build form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
+import { HomeComponent } from './pages/home/home.component';
+import {ProfileComponent} from './pages/profile/profile.component';
+import {LoginComponent} from './auth/login/login.component';
+import {RegisterComponent} from './auth/register/register.component';
+// Import other components...
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'home', component: HomeComponent },
+      { path: 'profile', component: ProfileComponent },
+      // Thêm các route con khác ở đây
+    ]
+  },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent }
+];
